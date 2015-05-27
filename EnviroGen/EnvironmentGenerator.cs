6e060a0f79cdf4db86bcbec2ae6d7662d04e897d
DeanJohnson <none@none>
2015-05-27 00:35:56 -0700

    Now has a size property

--- EnviroGen/EnvironmentGenerator.cs
+++ EnviroGen/EnvironmentGenerator.cs
@@ -4,23 +4,25 @@ namespace EnviroGen
 {
     class EnvironmentGenerator
     {
+        private Vector2i Size { get; set; }
         public Environment Environment;
 
-        public readonly HeightMapGenerator HeightGenerator;
+        public readonly PerlinHeightMapGenerator HeightMapGenerator;
         public readonly CloudGenerator CloudGenerator;
 
-        public EnvironmentGenerator(Vector2u size, int heightOctaveCount, int cloudOctaveCount)
+        public EnvironmentGenerator(Vector2i size, int heightOctaveCount, int cloudOctaveCount)
         {
-            HeightGenerator = new HeightMapGenerator(size, heightOctaveCount, Program.Random);
+            Size = size;
+            HeightMapGenerator = new PerlinHeightMapGenerator(size, heightOctaveCount, Program.Random);
             CloudGenerator = new CloudGenerator(size, cloudOctaveCount, Program.Random);
         }
 
         public void Generate()
         {
-            HeightGenerator.GenerateHeightMap();
+            HeightMapGenerator.GenerateHeightMap();
             CloudGenerator.GenerateCloudMap();
 
-            Environment = new Environment(new Terrain(HeightGenerator.HeightMap), 
+            Environment = new Environment(new Terrain(HeightMapGenerator.HeightMap, Size), 
                 new Clouds(CloudGenerator.CloudMap));
         }
     }
