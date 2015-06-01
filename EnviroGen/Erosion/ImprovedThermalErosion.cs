17ada8b3b0dcccb1075dab8665b1f8eea17bd6d8
DeanJohnson <none@none>
2015-06-01 13:58:44 -0700

    Added Thermal and Improved Thermal erosion methods

---
+++ EnviroGen/Erosion/ImprovedThermalErosion.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using SFML.Window;
+
+namespace EnviroGen.Erosion
+{
+    class ImprovedThermalErosion
+    {
+        public static void Erode(HeightMap heightMap, float talusAngle, int iterations)
+        {
+            for (var i = 0; i < iterations; i++)
+            {
+                for (var y = 0; y < heightMap.Size.Y; y++)
+                {
+                    for (var x = 0; x < heightMap.Size.X; x++)
+                    {
+                        var neighbors = heightMap.GetVonNeumannNeighbors(x, y);
+                        float highestSlope;
+                        var highestSlopedNeighbor = GetHighestSlopedNeighbor(heightMap, x, y, neighbors, out highestSlope);
+
+                        if (highestSlope <= talusAngle)
+                        {
+                            var heightDif = heightMap[x, y] - heightMap[highestSlopedNeighbor];
+                            heightMap[x, y] -= heightDif / 2f;
+                            heightMap[highestSlopedNeighbor] += heightDif / 2f;
+                        }
+                    }
+                }
+            }
+        }
+
+        private static Vector2i GetHighestSlopedNeighbor(HeightMap heightMap, int x, int y, IReadOnlyList<Vector2i> neighbors, out float slope)
+        {
+            var slopedNeighbor = neighbors[0];
+            var highestSlope = 0f;
+
+            foreach (var neighbor in neighbors)
+            {
+                var currentSlope = Math.Abs(heightMap[x, y] - heightMap[neighbor.X, neighbor.Y]);
+                if (currentSlope > highestSlope)
+                {
+                    highestSlope = currentSlope;
+                    slopedNeighbor = neighbor;
+                }
+            }
+
+            slope = highestSlope;
+            return slopedNeighbor;
+        }
+    }
+}
