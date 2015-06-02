8be3e1819d347f14a5fd9c41fe18e9ce2489b68f
DeanJohnson <none@none>
2015-06-01 22:28:39 -0700

    Added Colorize and Clear methods

--- EnviroGen/Coloring/Colorizer.cs
+++ EnviroGen/Coloring/Colorizer.cs
@@ -13,6 +13,24 @@ namespace EnviroGen.Coloring
             m_colorRanges = new List<ColorRange>();
         }
 
+        /// <summary>
+        /// Returns an Image with coloring applied based on the given HeightMap
+        /// </summary>
+        public Image Colorize(HeightMap map)
+        {
+            var img = new Image(map.Size.X, map.Size.Y);
+
+            for (uint y = 0; y < map.Size.Y; y++)
+            {
+                for (uint x = 0; x < map.Size.X; x++)
+                {
+                    img.SetPixel(x, y, GetColor(map[x, y]));
+                }
+            }
+
+            return img;
+        }
+
         public void AddColorRange(Color lowColor, Color highColor, float lowHeight, float highHeight)
         {
             m_colorRanges.Add(new ColorRange(lowColor, highColor, lowHeight, highHeight));
@@ -32,6 +50,11 @@ namespace EnviroGen.Coloring
         {
             var color = m_colorRanges.First(cr => cr.InRange(height)).GetColor(height);
             return color;
+        }
+
+        public void Clear()
+        {
+            m_colorRanges.Clear();
         }
     }
 }
