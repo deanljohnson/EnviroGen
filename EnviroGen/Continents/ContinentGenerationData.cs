9078f8f683e1d69b33c8849c30b082d5171d9165
DeanJohnson <none@none>
2015-06-04 13:15:31 -0700

    Added to keep track of data for continent generation

---
+++ EnviroGen/Continents/ContinentGenerationData.cs
@@ -0,0 +1,23 @@
+﻿namespace EnviroGen.Continents
+{
+    public class ContinentGenerationData
+    {
+        public int NumContinents { get; set; }
+        public int MinimumContinentSize { get; set; }
+        public int MaximumContinentSize { get; set; }
+
+        public ContinentGenerationData()
+        {
+            NumContinents = 0;
+            MinimumContinentSize = 0;
+            MaximumContinentSize = 0;
+        }
+
+        public ContinentGenerationData(int numContinents, int minSize, int maxSize)
+        {
+            NumContinents = numContinents;
+            MinimumContinentSize = minSize;
+            MaximumContinentSize = maxSize;
+        }
+    }
+}
