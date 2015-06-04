72f4664c5eaedac0f66af06981aba0cd181102b1
DeanJohnson <none@none>
2015-06-04 13:19:03 -0700

    Extracted some data to separate data classes

--- EnviroGen/GenerationOptions.cs
+++ EnviroGen/GenerationOptions.cs
@@ -8,9 +8,6 @@ namespace EnviroGen
         public int SizeY { get; set; }
         public int HeightMapOctaveCount { get; set; }
         public int CloudMapOctaveCount { get; set; }
-        public int NumContinents { get; set; }
-        public int MinimumContinentSize { get; set; }
-        public int MaximumContinentSize { get; set; }
         public float SeaLevel { get; set; }
         public float SandDistance { get; set; }
         public float ForestDistance { get; set; }
@@ -19,8 +16,6 @@ namespace EnviroGen
         public int CloudMapSeed { get; set; }
         public float NoiseRoughness { get; set; }
         public float NoiseFrequency { get; set; }
-        public float ErosionAngle { get; set; }
-        public int ErosionIterations { get; set; }
         public Colorizer TerrainColorizer { get; set; }
 
         public GenerationOptions()
@@ -29,9 +24,6 @@ namespace EnviroGen
             SizeY = 800;
             HeightMapOctaveCount = 8;
             CloudMapOctaveCount = 8;
-            NumContinents = 1;
-            MinimumContinentSize = 400;
-            MaximumContinentSize = 450;
             SeaLevel = .4f;
             SandDistance = .45f;
             ForestDistance = .75f;
@@ -40,8 +32,6 @@ namespace EnviroGen
             CloudMapSeed = -1;
             NoiseRoughness = .55f;
             NoiseFrequency = .005f;
-            ErosionAngle = .022f;
-            ErosionIterations = 40;
             TerrainColorizer = new Colorizer();
         }
     }
