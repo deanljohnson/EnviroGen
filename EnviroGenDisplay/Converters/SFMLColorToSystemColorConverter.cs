ff951681c6928a6090380f7f54ef8609b2a5bf0c
DeanJohnson <none@none>
2015-06-05 00:53:23 -0700

    Added color converter to convert between SFML's colors and c#'s built in colors

---
+++ EnviroGenDisplay/Converters/SFMLColorToSystemColorConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using SFML.Graphics;
+
+namespace EnviroGenDisplay.Converters
+{
+    [ValueConversion(typeof(Color), typeof(System.Windows.Media.Color))]
+    class SFMLColorToSystemColorConverter : BaseConverter, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var sfmlColor = (Color)value;
+            return System.Windows.Media.Color.FromArgb(sfmlColor.A, sfmlColor.R, sfmlColor.G, sfmlColor.B);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var sysColor = (System.Windows.Media.Color)value;
+            return new Color(sysColor.R, sysColor.G, sysColor.B, sysColor.A);
+        }
+    }
+}
