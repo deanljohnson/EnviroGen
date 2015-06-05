00caec7d13b3f9a35e7412686c92ddc8795ec83b
DeanJohnson <none@none>
2015-06-05 00:30:36 -0700

    Added basic converter classes for gui input processing

---
+++ EnviroGenDisplay/Converters/FloatToStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace EnviroGenDisplay.Converters
+{
+    class FloatToStringConverter : BaseConverter, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return float.Parse(value.ToString());
+        }
+    }
+}
---
+++ EnviroGenDisplay/Converters/IntToStringConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace EnviroGenDisplay.Converters
+{
+    class IntToStringConverter : BaseConverter, IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return int.Parse(value.ToString());
+        }
+    }
+}
