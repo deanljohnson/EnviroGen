0f4086093cdddfe30510bd7cf945ddcc0887058c
DeanJohnson <none@none>
2015-06-05 23:39:24 -0700

    Added a ridged option to change height map generation

--- EnviroGen/HeightMaps/HeightMapGenerator.cs
+++ EnviroGen/HeightMaps/HeightMapGenerator.cs
@@ -4,9 +4,9 @@ namespace EnviroGen.HeightMaps
 {
     public static class HeightMapGenerator
     {
-        public static HeightMap GenerateHeightMap(int sizeX, int sizeY, int octaveCount, float roughness, float frequency, int seed)
+        public static HeightMap GenerateHeightMap(int sizeX, int sizeY, int octaveCount, float roughness, float frequency, int seed, bool ridged = false)
         {
-            return new HeightMap(SimplexNoiseGenerator.GenerateNoiseArray(sizeX, sizeY, octaveCount, roughness, frequency, seed));
+            return new HeightMap(SimplexNoiseGenerator.GenerateNoiseArray(sizeX, sizeY, octaveCount, roughness, frequency, seed, ridged));
         }
     }
 }
