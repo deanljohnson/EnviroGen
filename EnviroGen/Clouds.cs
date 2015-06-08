74d3e5b1c305e146a53cac893867035654f06c04
DeanJohnson <none@none>
2015-06-07 21:19:25 -0700

    Changed to inherit from HeightMapDrawable

--- EnviroGen/Clouds.cs
+++ EnviroGen/Clouds.cs
@@ -1,67 +1,28 @@
-﻿using System;
+﻿using EnviroGen.Coloring;
 using EnviroGen.HeightMaps;
 using SFML.Graphics;
 
 namespace EnviroGen
 {
-    public class Clouds : Transformable, Drawable
+    public class Clouds : HeightMapDrawable
     {
-        private Sprite m_cloudSprite { get; set; }
-        private HeightMap m_cloudMap;
-
-        /// <summary>
-        /// Sets the Clouds height map. Setting this will cause a regeneration of the sprite.
-        /// </summary>
-        public HeightMap CloudMap
+        public Clouds(HeightMap heightMap)
+            : this(heightMap, DefaultColorizer)
         {
-            get { return m_cloudMap; }
-            set
-            {
-                m_cloudMap = value;
-                GenerateCloudSprite();
-            }
         }
 
-        public Clouds(HeightMap cloudMap)
+        public Clouds(HeightMap heightMap, Colorizer colorizer)
         {
-            m_cloudMap = cloudMap;
-            GenerateCloudSprite();
+            Colorizer = colorizer;
+
+            m_heightMap = heightMap;
+            Colorize(colorizer);
         }
 
-        public void Draw(RenderTarget target, RenderStates states)
+        public override void Draw(RenderTarget target, RenderStates states)
         {
             states.Transform.Combine(Transform);
-            target.Draw(m_cloudSprite, states);
-        }
-
-        /// <summary>
-        /// Generates a cloud sprite basd on the values from the cloud map
-        /// </summary>
-        /// <returns></returns>
-        private void GenerateCloudSprite()
-        {
-            var cloudImage = new Image(m_cloudMap.Size.X, m_cloudMap.Size.Y);
-            SetCloudPixels(cloudImage);
-            m_cloudSprite = new Sprite(new Texture(cloudImage));
-        }
-
-        /// <summary>
-        /// Sets image pixels to varying levels of white
-        /// </summary>
-        /// <param name="img"></param>
-        private void SetCloudPixels(Image img)
-        {
-            var cloudColor = new Color(255, 255, 255);
-
-            for (uint j = 0; j < img.Size.Y; j++)
-            {
-                for (uint i = 0; i < img.Size.X; i++)
-                {
-                    var height = (byte)((m_cloudMap[i, j] * m_cloudMap[i, j]) * Byte.MaxValue);
-                    cloudColor.A = (byte)(height / 3.5);
-                    img.SetPixel(i, j, cloudColor);
-                }
-            }
+            target.Draw(m_sprite, states);
         }
     }
 }
