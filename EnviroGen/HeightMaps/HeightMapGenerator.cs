6bacc625721019bbc5675928d06fc058843a3c4a
DeanJohnson <none@none>
2015-06-07 21:19:04 -0700

    Began implementing noise modifier support. Now handles FBM calls.

--- EnviroGen/HeightMaps/HeightMapGenerator.cs
+++ EnviroGen/HeightMaps/HeightMapGenerator.cs
@@ -1,12 +1,48 @@
-﻿using EnviroGen.Noise;
+﻿using System;
+using EnviroGen.Noise;
+using EnviroGen.Noise.Modifiers;
 
 namespace EnviroGen.HeightMaps
 {
     public static class HeightMapGenerator
     {
-        public static HeightMap GenerateHeightMap(int sizeX, int sizeY, int octaveCount, float roughness, float frequency, int seed, bool ridged = false)
+        public static HeightMap GenerateHeightMap(int sizeX, int sizeY, int octaveCount, float gain, float frequency, int seed, ModifierOptions modOptions = null)
         {
-            return new HeightMap(SimplexNoiseGenerator.GenerateNoiseArray(sizeX, sizeY, octaveCount, roughness, frequency, seed, ridged));
+            var arr = new float[sizeX, sizeY];
+
+            for (var y = seed; y < sizeY + seed; y++)
+            {
+                for (var x = seed; x < sizeX + seed; x++)
+                {
+                    arr[x - seed, y - seed] = FractalBrownianMotion.GenerateNoise(x, y, octaveCount, gain, frequency, 2f,
+                        SimplexNoiseGenerator.Noise2d);
+                }
+            }
+
+            if (modOptions != null) ApplyModifiers(ref arr, modOptions);
+
+            var map = new HeightMap(arr);
+            map.Normalize();
+
+            return map;
+        }
+
+        private static void ApplyModifiers(ref float[,] arr, ModifierOptions modOptions)
+        {
+            if (modOptions.Ridged)
+            {
+                RidgedModifier.Modify(ref arr);
+            }
+
+            if (Math.Abs(modOptions.Exponent - 1f) > float.Epsilon)
+            {
+                ExponentModifier.Modify(ref arr, modOptions.Exponent);
+            }
+
+            if (Math.Abs(modOptions.Scale - 1f) > float.Epsilon)
+            {
+                ScaleModifier.Modify(ref arr, modOptions.Scale);
+            }
         }
     }
 }
