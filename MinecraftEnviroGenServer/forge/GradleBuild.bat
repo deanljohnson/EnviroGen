6be279681c78260487179f53c32dc597d038dc62
deanljohnson <dean@deanljohnson.com>
2016-02-03 02:03:11 -0800

    Moved forge directory into MinecraftEnviroGenServer project. Work on the Java side of DELETE_BLOCK and SET_BLOCK commands.

---
+++ MinecraftEnviroGenServer/forge/GradleBuild.bat
@@ -0,0 +1,2 @@
+CALL gradlew build
+echo f | xcopy "build\libs\dean.minecraft.EnviroGen-1.0.jar" "..\bin\Debug\mods\dean.minecraft.EnviroGen-1.0.jar" /y
--- forge/GradleBuild.bat
+++
@@ -1,2 +0,0 @@
-CALL gradlew build
-echo f | xcopy "build\libs\dean.minecraft.EnviroGen-1.0.jar" "..\MinecraftEnviroGenServer\bin\Debug\mods\dean.minecraft.EnviroGen-1.0.jar" /y
